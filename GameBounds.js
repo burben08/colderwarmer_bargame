6795ec2212e5cb9d73c34e7b1387e8e0e739c69e
burben08 <mynoxlps@gmail.com>
2025-10-28 14:55:03 +0100

    Add files via upload
    GAME BOUNDS for different regions

---
+++ GameBounds.js
@@ -0,0 +1,58 @@
+export let GAME_BOUNDS_Regions = {
+  Switzerland: {
+    south: 45.817,
+    north: 47.808,
+    west: 5.955,
+    east: 10.492,
+  },
+
+  Europe: {
+    south: 34.5,
+    north: 71.2,
+    west: -25.0,
+    east: 40.0,
+  },
+
+  Germany: {
+    south: 47.270111,
+    north: 55.058347,
+    west: 5.866342,
+    east: 15.041896,
+  },
+
+    Spain: {
+    south: 36.0,
+    north: 43.8,
+    west: -9.3,
+    east: 3.3,
+  },
+
+  Italy: {
+    south: 36.65,
+    north: 47.09,
+    west: 6.62,
+    east: 18.98,
+  },
+
+  France: {
+    south: 41.33,
+    north: 51.09,
+    west: -5.14,
+    east: 9.56,
+  },
+
+  Austria: {
+    south: 46.37,
+    north: 49.02,
+    west: 9.53,
+    east: 17.16,
+  },
+
+  Asia: {
+    south: -10.0,
+    north: 55.0,
+    west: 25.0,
+    east: 180.0,
+  },
+};
+
